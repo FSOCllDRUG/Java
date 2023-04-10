2e652558c9e98dbb3143493d5a5421d687cd6b6c
Vlad <vlad.a.borshch@gmail.com>
2023-04-10 16:50:47 +0300

    HW6

---
+++ s6h/Main.java
@@ -0,0 +1,15 @@
+package s6h;
+
+import s6h.model.Data;
+import s6h.model.Filter2;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        Filter.filterLaptopSet(Data.baseLaptopSet());
+
+        Filter2.filterLaptopSet(Data.baseLaptopSet());
+
+    }
+}
+
