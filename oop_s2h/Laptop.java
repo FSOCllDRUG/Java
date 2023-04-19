fb0080eef4635b63edc22039aa1953c30a070e7b
Vlad <vlad.a.borshch@gmail.com>
2023-04-20 01:36:21 +0300

    OOP HW 2

---
+++ oop_s2h/Laptop.java
@@ -0,0 +1,15 @@
+package oop_s2h;
+
+public class Laptop {
+    private Integer _ram;
+    private Integer _hd;
+    private String _os;
+    private String _color;
+
+    public Laptop(int ram, int hd, String os, String color) {
+        _ram = ram;
+        _hd = hd;
+        _os = os;
+        _color = color;
+    }
+}
---
+++ oop_s2h/Main.java
@@ -0,0 +1,10 @@
+package oop_s2h;
+
+import oop_s2h.model.Store;
+
+public class Main {
+    public static void main(String[] args) {
+        Store store = new Store(5);
+        store.findLaptop();
+    }
+}
